e6eb3e0a5ba617e47a7d5366b28a63bc2496749d
Gonzalo <gonzacalleja@gmail.com>
2019-12-11 23:23:16 +0100

    commit

--- HappinessAnalysis.py
+++ HappinessAnalysis.py
@@ -1,5 +1,7 @@
 from pyspark import SparkConf, SparkContext
+import re
 
+DATA_POINTS = ["Country", "Region", "Score"]
 
 # method extract country name, region & happiness score
 def parseLine(line, columns):
@@ -11,13 +13,31 @@ def parseLine(line, columns):
             result.append(field)
 
     for index, element in enumerate(result):
-        if element.replace('.', '', 1).isdigit():
+        if re.search("/d", element):
             result[index] = float(element)
         else:
-            result[index] = str(element)
+            result[index] = str(re.sub('"', "", element))
 
     return result
 
+# Since not all files are structured the same way we have to organize them:
+def cleanFile(lines):
+    header = lines.first().split(',')
+    columns = []
+
+    for index, columnName in enumerate(header):
+        for dataPoint in DATA_POINTS:
+            if dataPoint in columnName:
+                columns.append(index)
+
+    return lines.map(lambda row: parseLine(row, columns)).filter(lambda x: x[0] != DATA_POINTS[0])
+
+
+def flipKeyValue(line):
+    if len(line) == 3:
+        return line[2], line[1], line[0]
+    elif len(line) == 2:
+        return line[1], line[0]
 
 # Create Spark context and name the project
 conf = SparkConf().setMaster("local").setAppName("HappinessProjectSPARK")
@@ -28,75 +48,31 @@ sc = SparkContext(conf=conf)
 
 # LOAD DATA
 data_files = {
-    "2015" : sc.textFile("./dataFiles/2015.csv"),
-    "2016" : sc.textFile("./dataFiles/2016.csv"),
-    "2017" : sc.textFile("./dataFiles/2017.csv"),
-    "2018" : sc.textFile("./dataFiles/2018.csv"),
-    "2019" : sc.textFile("./dataFiles/2019.csv")
-
+    "2015" : cleanFile(sc.textFile("./dataFiles/2015.csv")),
+    "2016" : cleanFile(sc.textFile("./dataFiles/2016.csv")),
+    "2017" : cleanFile(sc.textFile("./dataFiles/2017.csv")),
+    "2018" : cleanFile(sc.textFile("./dataFiles/2018.csv")),
+    "2019" : cleanFile(sc.textFile("./dataFiles/2019.csv"))
 }
 
 
-# Since not all files are structured the same way we
-def cleanFile(lines):
-    header = lines.first().split(',')
-
-    dataPoints = ["Country", "Region", "Score"]
-    columns = []
-
-    for index, columnName in enumerate(header):
-        for dataPoint in dataPoints:
-            if dataPoint in columnName:
-                columns.append(index)
-
-    rdd = lines.map(lambda row: parseLine(row, columns)).filter(lambda x: x[0] != dataPoints[0])
-
-    return rdd
-
-# .filter(lambda x: x[0] != DataPoints[0])
-
 # FIRST ANALYSIS: Happiest country for each year
-lines = sc.textFile("./dataFiles/2015.csv")
+for year, rdd in data_files.items():
 
-a = cleanFile(lines).sortByKey().collect()
-for i in a:
-    print(i)
+    alphabetically = rdd.sortByKey()
+    # flipped = rdd.map(flipKeyValue)
+    # sortedRDD = flipped.sortByKey().map(flipKeyValue)
+    results = alphabetically.collect()
+    print(year)
+    #print(results[-1])
 
-# for year, lines in data_files.items():
-#     print(year)
-#
-#     header = lines.first().split(',')
-#     for
-#
-#     # Create the RDD: 1) extract the 3 values we want 2) take out the header
-#     rdd = lines.map(parseLine).filter(lambda x: x[0] != header)
-#
-#     alphabetically = rdd.sortByKey()
-#
-#     results = alphabetically.collect()
-#
-#     for result in results:
-#         print(result)
+    for result in results:
+        print(result)
 
-# rdd = lines_2015.map(parseLine).filter(lambda x: x[0]!=header)
-# happinessPerCountry = rdd.sortByKey()
-#
-# # reverse the key and the value, it swops x for y, and y for x
-# flipped = happinessPerCountry.map(lambda x : (x[1], x[0]))
-# sortedHappinessPerCountry = flipped.sortByKey().map(lambda x : (x[1], x[0]))
-# resultsHappinessPerCountry = sortedHappinessPerCountry.collect()[-1]
-# print "The happiest country is: ", str(resultsHappinessPerCountry[0]), "with a happiness score of: ", resultsHappinessPerCountry[1]
-# print "The happiest country is: ", str(resultsHappinessPerCountry[0]), "with a happiness score of: ", resultsHappinessPerCountry[1]
-#
-# #for key, value in resultsHappinessPerCountry:
-#     #print(str(key), value)
-#
-#
-#
+
+
 # # happiest year of a country:
 # happiest = flipped.reduceByKey(lambda x, y: max(x,y))
 # result = happiest.collect()
 # #print(result)
-#
-# #for resul in result:
-#     #print(resul[0])
+
