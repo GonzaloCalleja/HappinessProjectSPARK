2b29a668d49520a8adbacf731b7254378c0546f4
ClaraDubois <cdubois.ieu2017@student.ie.edu>
2019-12-11 19:44:01 +0100

    second commit

--- HappinessAnalysis.py
+++ HappinessAnalysis.py
@@ -7,6 +7,7 @@ sc = SparkContext(conf = conf)
 
 #LOAD DATA
 lines_2015 = sc.textFile("./dataFiles/2015.csv")
+header_country = lines_2015.first().split(',')[0]
 lines_2016 = sc.textFile("./dataFiles/2016.csv")
 lines_2017 = sc.textFile("./dataFiles/2017.csv")
 lines_2018 = sc.textFile("./dataFiles/2018.csv")
@@ -16,22 +17,29 @@ lines_2019 = sc.textFile("./dataFiles/2019.csv")
 def parseLine(line):
     fields = line.split(',')
     country = fields[0]
-    #if country == ("Country"):
-        #return (country, 0)
+    if country == ("Country"):
+        return (country, 0)
     happiness = float(fields[3])
     return (country, happiness)
 
-rdd = lines_2015.map(parseLine)
+rdd = lines_2015.map(parseLine).filter(lambda x : (x[0] != header_country))
 happinessPerCountry = rdd.sortByKey()
 
-# reverse the key and the value, it swops x for y, and y for x
+# reverse the key and the value, it swaps x for y, and y for x
 flipped = happinessPerCountry.map(lambda x : (x[1], x[0]))
 sortedHappinessPerCountry = flipped.sortByKey().map(lambda x : (x[1], x[0]))
 resultsHappinessPerCountry = sortedHappinessPerCountry.collect()
-counter = 0
 
+# DEFINE HAPPIEST COUNTRY PER YEAR (year = 2015)
+happiest_country = flipped.reduceByKey(lambda x, y: max(x, y))
+print happiest_country.collect()
+
+
+
+
+
+"""
 for key, value in resultsHappinessPerCountry:
-    #if counter != 0:
-    print key, value
+        print key, value
+ """
 
-    counter+=1
